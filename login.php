7ab7413410477e4eefd5af31c617b1fe01c9d5df
nash <jonashdeguia13@gmail.com>
2026-06-25 20:10:30 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login</title>
+    <link href="./output.css" rel="stylesheet">
+</head>
+
+<body class="bg-slate-950 text-white flex items-center justify-center h-screen">
+
+<div class="bg-white/5 p-8 rounded-xl w-80">
+
+    <h1 class="text-2xl font-bold mb-6">Login</h1>
+
+    <input placeholder="Email" class="w-full p-2 mb-3 text-black rounded">
+    <input placeholder="Password" class="w-full p-2 mb-3 text-black rounded">
+
+    <button class="bg-blue-600 w-full py-2 rounded">
+        Login
+    </button>
+
+</div>
+
+</body>
+</html>
